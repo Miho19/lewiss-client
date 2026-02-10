c4571cb8e221aa9133d2d5139c9337c5a9959ea1
miho19 <miho19@hotmail.co.nz>
2026-02-11 02:16:01 +1300

    some responsive design for customer search form. add in react form hook later

--- src/components/customer/CustomeSearchForm.tsx
+++ src/components/customer/CustomeSearchForm.tsx
@@ -23,38 +23,49 @@ function CustomerSearchForm(props: Props) {
   }
 
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full flex flex-col md:items-center space-y-6">
       <h2 className="border-b border-black/5 py-6">Customer Search</h2>
-      <form className="flex flex-col space-y-6">
-        <label className="">
+      <form className="grid grid-cols-5 gap-4 md:grid-cols-3 md:grid-rows-2 ">
+        <label htmlFor="familyName" className="col-span-2 md:col-span-1 md:row-start-1">
           Family name:
-          <input
-            className="bg-gray-100 border border-white rounded-lg ml-3"
-            name="familyName"
-            onChange={handleInputOnChange}
-            value={customerSearchData.familyName}
-          />
         </label>
-        <label>
-          Mobile number:
-          <input
-            className="bg-gray-100 border border-white rounded-lg ml-3"
-            name="mobileNumber"
-            onChange={handleInputOnChange}
-            value={customerSearchData.mobileNumber}
-          />
-        </label>
+        <input
+          id="familyName"
+          className="bg-gray-100 border border-white rounded-lg ml-3 col-span-3 md:col-span-1 md:row-start-2 md:ml-0"
+          name="familyName"
+          onChange={handleInputOnChange}
+          value={customerSearchData.familyName}
+        />
 
-        <label>
-          Email address:
-          <input
-            className="bg-gray-100 border border-white rounded-lg ml-3"
-            type="email"
-            name="emailAddress"
-            onChange={handleInputOnChange}
-            value={customerSearchData.emailAddress}
-          />
+        <label htmlFor="mobileNumber" className="col-span-2 md:col-span-1 md:row-start-1">
+          Mobile number:
         </label>
+        <input
+          id="mobileNumber"
+          className="bg-gray-100 border border-white rounded-lg ml-3 col-span-3 md:col-span-1 md:row-start-2 md:ml-0"
+          name="mobileNumber"
+          onChange={handleInputOnChange}
+          value={customerSearchData.mobileNumber}
+        />
+
+        <label htmlFor="emailAddress" className="col-span-2 md:col-span-1 md:row-start-1">
+          Email address:
+        </label>
+        <input
+          id="emailAddress"
+          className="bg-gray-100 border border-white rounded-lg ml-3 col-span-3  md:col-span-1 md:row-start-2 md:ml-0"
+          type="email"
+          name="emailAddress"
+          onChange={handleInputOnChange}
+          value={customerSearchData.emailAddress}
+        />
+
+        <button
+          type="submit"
+          className="border border-black rounded-lg col-span-full md:col-span-1 md:w-24"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
