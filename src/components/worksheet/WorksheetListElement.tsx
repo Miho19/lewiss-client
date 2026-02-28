d2876d66cc9844b9be98b761bba7a2faeeda11f7
miho19 <miho19@hotmail.co.nz>
2026-02-28 22:01:29 +1300

    format of numbers for worksheet

--- src/components/worksheet/WorksheetListElement.tsx
+++ src/components/worksheet/WorksheetListElement.tsx
@@ -5,8 +5,26 @@ type Props = {
 };
 
 function WorksheetListElement(props: Props) {
+  const { worksheet } = props;
+
+  const priceFormatted = worksheet.price.toLocaleString('en-us', {
+    style: 'currency',
+    currency: 'NZD',
+  });
+
+  const discountFormatted = worksheet.discount.toLocaleString('en-us', {
+    style: 'currency',
+    currency: 'NZD',
+  });
+
   return (
-    <li className="flex flex-1 w-full flex-col p-3 shadow-md sm:rounded-lg hover:bg-gray-100 transition-all duration-100 ease-in-out group cursor-pointer hover:-translate-y-3 border-t border-black/5"></li>
+    <li
+      className="flex flex-1 w-full flex-col p-3 shadow-md sm:rounded-lg 
+    hover:bg-gray-100 transition-all duration-100 ease-in-out group cursor-pointer hover:-translate-y-3 border-t border-black/5"
+    >
+      <p>Price: {}</p>
+      <p className="text-xs italic text-gray-400 border-t border-black/5 mt-2">{worksheet.id}</p>
+    </li>
   );
 }
 
