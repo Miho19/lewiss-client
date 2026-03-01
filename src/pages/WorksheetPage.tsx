7a7bff5ae8d1abb8677eb0ffba6e297d904f25be
miho19 <miho19@hotmail.co.nz>
2026-03-01 23:11:51 +1300

    styled worksheet page, added effect to check if selected customer is valid

--- src/pages/WorksheetPage.tsx
+++ src/pages/WorksheetPage.tsx
@@ -1,4 +1,8 @@
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
+import ButtonGrey from '../components/common/ButtonGrey';
+import { CircleArrowLeft } from 'lucide-react';
+import { useEffect } from 'react';
+import useCustomerSelect from '../hooks/useCustomerSelect';
 
 type WorksheetPageParams = {
   worksheetId: string;
@@ -6,8 +10,35 @@ type WorksheetPageParams = {
 
 function WorksheetPage() {
   const params = useParams<WorksheetPageParams>();
+  const navigator = useNavigate();
+  const selectedCustomer = useCustomerSelect();
 
-  return <div>{params.worksheetId}</div>;
+  useEffect(() => {
+    if (typeof selectedCustomer.customer === 'undefined') {
+      navigator('/customer');
+    }
+  }, []);
+
+  function backButtonOnClickHandler() {
+    navigator('/customer');
+  }
+  // use react query to get worksheet from backend...
+
+  return (
+    <div className="w-full h-full flex flex-col items-center space-y-10">
+      <div className="flex w-full justify-end">
+        <ButtonGrey
+          buttonText="Back"
+          icon={CircleArrowLeft}
+          onClickHandler={backButtonOnClickHandler}
+        />
+      </div>
+
+      <div className="w-full h-full flex flex-col space-y-6 shadow-md px-3 border-t border-black/5">
+        {params.worksheetId}
+      </div>
+    </div>
+  );
 }
 
 export default WorksheetPage;
