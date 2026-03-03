a62c35b14bee685da17ff084eb14529ed83336b7
miho19 <miho19@hotmail.co.nz>
2026-03-03 20:06:32 +1300

    switched back to a unique component because the search result element looked strange

--- src/components/customer/CustomerInformation.tsx
+++ src/components/customer/CustomerInformation.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import useCustomerSelect from '../../hooks/useCustomerSelect';
 import { useNavigate } from 'react-router';
-import CustomerSearchResultElement from './CustomerSearchResultElement';
 import ButtonGrey from '../common/ButtonGrey';
 import { UserPen } from 'lucide-react';
 
@@ -20,8 +19,17 @@ function CustomerInformation() {
   function editButtonOnClickHandler() {}
 
   return (
-    <div className="flex w-full flex-col space-y-3">
-      <CustomerSearchResultElement customer={customerSelected.customer} hoverable={false} />
+    <div className="flex w-full flex-col space-y-3 shadow-md p-3 border-t border-black/5">
+      <div className="w-full flex flex-col space-y-4 p-3">
+        <p className="text-lg font-semibold border-b border-black/15 pb-3">
+          {customerSelected.customer.familyName}
+        </p>
+        <p className="text-sm">{customerSelected.customer.mobile}</p>
+        <p className="text-sm">{customerSelected.customer.email}</p>
+        <p className="text-sm">{customerSelected.customer.street}</p>
+        <p className="text-sm">{customerSelected.customer.suburb}</p>
+        <p className="text-sm">{customerSelected.customer.city}</p>
+      </div>
       <div className="flex w-full justify-end">
         <ButtonGrey buttonText="Edit" onClickHandler={editButtonOnClickHandler} icon={UserPen} />
       </div>
